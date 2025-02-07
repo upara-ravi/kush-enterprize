240270246c658168514a87c5ae5e442389c9268a
ravi upara <upararavi25@gmail.com>
2025-02-07 14:12:22 +0530

    changes

---
+++ kush-enterprize/src/component/header.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './header.css'
+
+
+
+function Header() {
+    return (
+        <>
+            <div className='header'>
+                Header
+            </div>
+        </>
+    )
+}
+export default Header
--- kush-enterprize/src/main.jsx
+++ kush-enterprize/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
+import Header from './component/header.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>,
+
 )
